68a1e9bf62958eacdba7b4feff4e1c500dc0e073
pi22-a <pi22a555@naver.com>
2022-10-28 14:25:46 +0900

    Dependency

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,45 +1,37 @@
 package com.springboot.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController //스프링이 컨트롤러로 인지함
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
-
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String getHello(){
+    public String hello(){
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
-    public String getName(){
-        return "Flature";
+    public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
+        return "pizza";
     }
 
     @GetMapping(value = "/variable1/{variable}")
-    public String getVariable(@PathVariable String variable){
+    public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다. variable:{}", variable);
         return variable;
-    }
-
-    @GetMapping(value = "/variable2/{variable}")
-    public String getVariable2(@PathVariable ("variable") String var){
-        return var;
-    }
-
-    @GetMapping(value = "/requeset1")
-    public  String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String organization){
-        return name + " " + email + " " + organization;
-    }
-
-    @GetMapping(value = "/requeset2")
-    public  String getRequestParam2(@RequestParam Map<String, String> param){
 
     }
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
+
 }
 
 
